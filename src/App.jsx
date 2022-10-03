e05a0c5b884e65f6f4163db607573f62ec57f936
Carlos Vivas <carlosmuerto@gmail.com>
2022-10-03 18:20:41 -0400

    Add Base Navagable Pages

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from 'react-router-dom';
+import React from 'react';
+import Header from './view/components/Header/header';
+import HomePage from './view/pages/Home/Home';
+import DetailsPage from './view/pages/Details/Details';
+
+const links = [
+	{
+		path: '/',
+		text: 'Home',
+		element: (<HomePage />),
+	},
+	{
+		path: '/Missions',
+		text: 'Details',
+		element: (<DetailsPage />),
+	},
+];
+const App = () => (
+	<div className="app">
+		<Router>
+			<Header links={links} />
+			<Routes>
+				{links.map((link) => (
+					<Route
+						key={`RouteTo:${link.text}`}
+						path={link.path}
+						element={link.element}
+					/>
+				))}
+			</Routes>
+		</Router>
+	</div>
+);
+
+export default App;
---
+++ src/view/pages/Details/Details.jsx
@@ -0,0 +1,7 @@
+const DetailsPage = () => (
+	<main className="app-details">
+		<h1>Details Page</h1>
+	</main>
+);
+
+export default DetailsPage;
